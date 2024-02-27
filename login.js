1bd658860964fc79c06a5c3e695f61bb7da6b0f7
PirateQueen890 <piratequeen890@gmail.com>
2024-02-26 23:30:11 -0700

    share functions & promises

--- login.js
+++ login.js
@@ -2,4 +2,5 @@ function login() {
     const currentUsername = document.querySelector("#username");
     localStorage.setItem("username", currentUsername.value);
     window.location.href = "generator.html";
+    console.log("hello");
 }
--- share.js
+++ share.js
@@ -1,3 +1,42 @@
 function share() {
-    window.location.href = "share.html";
+    const shareUsername = document.querySelector("#friendUsername");
+    localStorage.setItem("shareUsername", shareUsername.value);
+
+    const request = createRequest();
+
+    sendRequest(request)
+    .then((request) => returnRequest(request))
+    .catch((request) => {
+        failedRequest(request);
+    });
+}
+
+function createRequest(request) {
+    const requestElement = document.createElement("li");
+    request = { element: requestElement, id: localStorage.getItem("shareUsername") };
+
+    requestElement.innerHTML = `<span>[${request.id}] 😄 <i>Sending</i> ...</span>`;
+    const requests = document.getElementById("requests");
+    requests.appendChild(requestElement);
+
+    return request;
+}
+
+function sendRequest(request) {
+    return new Promise((resolve, reject) => {
+        webSocketRequest(request, resolve, reject);
+      });
+}
+
+function returnRequest(request) {
+    request.element.innerHTML = `<span>[${request.id}] 🥳 <b>Sent</b>!</span>`;
+}
+
+function webSocketRequest(request, resolve, reject) {
+    //Websocket
+    resolve(request);
+}
+
+function failedRequest(request) {
+    request.element.innerHTML = `<span>[${request.id}] 😔 <b>Failed</b>!</span>`;
 }
