9efeff0be73ddf4a8b571f59ece84982a7d50363
PirateQueen890 <piratequeen890@gmail.com>
2024-03-22 22:57:44 -0600

    added favorite and received functions

--- database.js
+++ database.js
@@ -5,10 +5,28 @@ const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db("simon");
+const db = client.db("prompt-generator");
 const userCollection = db.collection("user");
 const favoritesCollection = db.collection("favorites");
 const receivedCollection = db.collection("received");
+
+let favorites = [
+    { id: "save1", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "save2", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "save3", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "save4", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "save5", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "save6", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+  ];
+  
+let received = [
+    { id: "received1", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "received2", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "received3", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "received4", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "received5", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+    { id: "received6", owner: "", type: "", colors: ["rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)", "rgb(255, 255, 255)"], prompt: "" },
+];
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -36,16 +54,70 @@ async function createUser(username, password) {
         password: passwordHash,
         token: uuid.v4(),
     };
-    
+
     await userCollection.insertOne(user);
+
+    const newFavorites = {
+        username: username, 
+        favorites: favorites,
+    };
+
+    await favoritesCollection.insertOne(newFavorites);
+
+    const newReceived = {
+        username: username, 
+        received: received,
+    };
+
+    await receivedCollection.insertOne(newReceived);
 
     return user;
 }
 
+function updateFavorites(saves, username) {
+    const filter = {username: username};
+    const options = {upsert: false};
+    const update = {$set: saves};
 
+    favoritesCollection.updateOne(filter, update, options);
+}
+
+function updateReceived(received, username) {
+    const filter = {username: username};
+    const options = {upsert: false};
+    const update = {$set: received};
+
+    receivedCollection.updateOne(filter, update, options);
+}
+
+function getFavorites(username) {
+    const query = {username: username};
+    const options = {
+        projection: {username: 0, favorites: 1},
+    }
+
+    const userFavorites = await.favoritesCollection.findOne(query, options);
+
+    return userFavorites;
+}
+
+function getReceived(username) {
+    const query = {username: username};
+    const options = {
+        projection: {username: 0, received: 1},
+    }
+
+    const userReceived = await.receivedCollection.findOne(query, options);
+
+    return userReceived;
+}
 
 module.exports = {
     getUser,
     getUserByToken,
     createUser,
+    updateFavorites,
+    updateReceived,
+    getFavorites,
+    getReceived,
   };
