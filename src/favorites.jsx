551cf309c5504efe6d266bfdc74c31833db83db0
PirateQueen890 <piratequeen890@gmail.com>
2024-04-08 23:14:46 -0600

    view component stubs created

---
+++ src/favorites.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Favorites() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>favorites displayed here</div>
+    </main>
+  );
+}
