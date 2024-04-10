c7b2f8cda36e7364fcfb8b6be90fa4fae4c53180
PirateQueen890 <piratequeen890@gmail.com>
2024-04-09 19:08:33 -0600

    setting up router

--- src/app.jsx
+++ src/app.jsx
@@ -1,50 +1,67 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Generator } from './generator/generator';
+import { Favorites } from './favorites/favorites';
+import { About } from './about/about';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
     return (
-      <div className='body'>
-        <header className='container-fluid'>
-          <nav className='navbar fixed-top navbar-dark'>
-            <div className='navbar-brand'>
-              ClickInspiration<sup>&reg;</sup>
+        <BrowserRouter>
+            <div className='body'>
+                <header className='container-fluid'>
+                    <nav className='navbar fixed-top navbar-dark'>
+                        <div className='navbar-brand'>
+                        ClickInspiration<sup>&reg;</sup>
+                        </div>
+                        <menu className='navbar-nav'>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to="">
+                            Home
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='generator'>
+                            Generate
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='favorites'>
+                            Favorites
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='about'>
+                            About
+                            </NavLink>
+                        </li>
+                        </menu>
+                    </nav>
+                </header>
+        
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/generator' element={<Generator />} />
+                    <Route path='/favorites' element={<Favorites />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+        
+                <footer className="bg-dark text-white-50">
+                    <div className="container-fluid">
+                        <span className="text-reset">Author: Emma Bills</span>
+                    </div>
+                    <div className="container-fluid">
+                        <a className="text-reset" href="https://github.com/PirateQueen890/startup">Source</a>
+                    </div>
+                </footer>
             </div>
-            <menu className='navbar-nav'>
-              <li className='nav-item'>
-                <a className='nav-link' href='index.html'>
-                  Home
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='generator.html'>
-                  Generator
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='favorites.html'>
-                  Favorites
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='about.html'>
-                  About
-                </a>
-              </li>
-            </menu>
-          </nav>
-        </header>
-  
-        <main>App components go here</main>
-  
-        <footer className="bg-dark text-white-50">
-            <div className="container-fluid">
-                <span className="text-reset">Author: Emma Bills</span>
-            </div>
-            <div className="container-fluid">
-                <a className="text-reset" href="https://github.com/PirateQueen890/startup">Source</a>
-            </div>
-        </footer>
-      </div>
+        </BrowserRouter>
     );
+  }
+
+  function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
   }
