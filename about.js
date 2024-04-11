c1e7c6a158aec4efd922502c8551dd1f17226369
PirateQueen890 <piratequeen890@gmail.com>
2024-04-10 23:06:48 -0600

    moved about page into react

--- about.js
+++
@@ -1,9 +0,0 @@
-function loadPage() {
-    let username = localStorage.getItem("username");
-    username = JSON.parse(username);
-
-    if (username == null) {
-        console.log("Not logged in.")
-        document.getElementById("navFav").remove();
-    }
-}
